5d3e2aa9174101b73b2d9b94abc4f60ef999714e
Ryan Scott <ryankennethscott@gmail.com>
2018-11-14 21:29:14 +1300

    Add new types, make ID optional

--- src/kanflow/types.js
+++ src/kanflow/types.js
@@ -1,7 +1,7 @@
 // @flow
 
 export type Todo = {
-  ID: number,
+  ID?: number,
   name: string,
   description: string,
   status: string,
@@ -15,4 +15,44 @@ export type Todo = {
   last_sync_timestamp: Date,
   external_provider_ID: string,
   external_item_ID: string
+};
+
+export type Project = {
+  ID?: number,
+  name: string,
+  description: string,
+  status: string,
+  completed: boolean,
+  archived: boolean,
+  archived_timestamp: Date,
+  completed_timestamp: Date,
+  created_timestamp: Date,
+  last_updated_timestamp: Date
+};
+
+export type Status = {
+  ID?: number,
+  name: string,
+  description: string,
+  status: string,
+  archived: boolean,
+  archived_timestamp: Date,
+  created_timestamp: Date,
+  last_updated_timestamp: Date
+};
+
+export type Provider = {
+  ID?: number,
+  name: string,
+  created_timestamp: Date,
+  API_secret: string,
+  API_key: string
+};
+
+export type User = {
+  ID?: number,
+  name: string,
+  email: string,
+  verified: boolean,
+  password: string
 };
