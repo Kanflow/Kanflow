8b5e054a93394d905da6f078612c79f29d13160e
Ryan Scott <ryankennethscott@gmail.com>
2018-11-15 20:48:55 +1300

    Update Todoist to be a singleton

--- src/todoist/index.js
+++ src/todoist/index.js
@@ -1,38 +1,40 @@
 // @flow
 const fetch = require("node-fetch");
+require("dotenv").config();
 
 const TODOIST_BASE_URL = "https://beta.todoist.com/API/v8/";
-
-function getProjects(token: string) {
-  return fetch(`${TODOIST_BASE_URL}projects`, {
-    method: "GET",
-    headers: { Authorization: `Bearer ${token}` }
-  })
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      console.error(res.statusText); // TODO: Find a better way to handle errors
+const Todoist = {
+  token: process.env.TODOIST_API_KEY,
+  getProjects() {
+    return fetch(`${TODOIST_BASE_URL}projects`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${this.token}` }
     })
-    .catch(err => {
-      console.error("Error getting projects:", err); // TODO: Find a better way to handle errors
-    });
-}
-
-function getTasks(token: string) {
-  return fetch(`${TODOIST_BASE_URL}tasks`, {
-    method: "GET",
-    headers: { Authorization: `Bearer ${token}` }
-  })
-    .then(res => {
-      if (res.ok) {
-        return res.json();
-      }
-      console.error(res.statusText); // TODO: Find a better way to handle errors
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        console.error(res.statusText); // TODO: Find a better way to handle errors
+      })
+      .catch(err => {
+        console.error("Error getting projects:", err); // TODO: Find a better way to handle errors
+      });
+  },
+  getTasks() {
+    return fetch(`${TODOIST_BASE_URL}tasks`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${this.token}` }
     })
-    .catch(err => {
-      console.error("Error getting tasks:", err);
-    });
-}
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        }
+        console.error(res.statusText); // TODO: Find a better way to handle errors
+      })
+      .catch(err => {
+        console.error("Error getting tasks:", err);
+      });
+  }
+};
 
-module.exports = { getTasks, getProjects };
+module.exports = Todoist;
