1a3d511f6dd86c75422d5735a657e349d645cd8f
Ryan Scott <ryankennethscott@gmail.com>
2018-11-18 17:36:47 +1300

    Updating todo table to have foreign keys

--- migrations/20181111200052_create_todo_table.js
+++ migrations/20181111200052_create_todo_table.js
@@ -1,11 +1,11 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable("todo", function(t) {
+  return knex.schema.createTable("todo", t => {
     t.increments("ID")
       .unsigned()
       .primary();
     t.string("name");
     t.text("description");
-    t.boolean("status");
+    t.integer("status_ID");
     t.boolean("completed");
     t.boolean("archived");
     t.integer("project_ID");
@@ -16,6 +16,15 @@ exports.up = function(knex, Promise) {
     t.timestamp("last_sync_timestamp");
     t.string("external_provider_ID");
     t.string("external_item_ID");
+    t.foreign("status_ID")
+      .references("ID")
+      .inTable("status");
+    t.foreign("project_ID")
+      .references("ID")
+      .inTable("project");
+    t.foreign("external_provider_ID")
+      .references("ID")
+      .inTable("provider");
   });
 };
 
